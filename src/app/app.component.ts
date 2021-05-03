3257622ff8b4e5366d1fc6dad3ad030c623c5d9d
Caio Ladislau <caio@ladislau.me>
2021-05-03 16:27:54 -0300

    Implementa métodos ed login  e logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenAuthService } from './shared/services/token-auth.service';
+import { AuthenticationStateService } from './shared/services/authentication-state.service';
+import { JwtService } from './shared/services/jwt.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit
+{
   title = 'gestao';
+
+  isLoggedin: boolean|null;
+
+  constructor(
+    public authenticationStateService: AuthenticationStateService,
+    public router: Router,
+    private tokenAuthService: TokenAuthService,
+    private jwtService: JwtService
+  ) {
+    this.isLoggedin = null;
+  }
+
+  ngOnInit() {
+    this.authenticationStateService.userAuthState.subscribe(res => {
+        this.isLoggedin = res;
+    });
+  }
+
+  logOut() {
+    this.jwtService.logOut().subscribe(() => {
+      this.authenticationStateService.setAuthState(false);
+      this.tokenAuthService.destroyToken();
+      this.router.navigate(['signin']);
+    });
+  }  
 }
